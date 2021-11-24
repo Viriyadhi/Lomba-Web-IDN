4cdeb7c7ce58958459fa939f0bb7e2684631b380
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2021-11-24 21:53:04 +0700

    WIP(Card Map)[OF]: Adding Card Map click listener

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,146 +1,66 @@
-const cards = [
-    {
-        "detail": {
-            "p": "Jakarta Utara",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Jakarta Selatan",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Jakarta Timur",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Jakarta Barat",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Tangerang",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Bogor",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Bekasi",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Bandung",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Semarang",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Yogyakarta",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Surabaya",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-    {
-        "detail": {
-            "p": "Malang",
-            "img": false
-        },
-        "childrens": [
-
-        ]
-    },
-];
-
+var isClicked = false;
 const elements = [];
 
 const cardContainer = document.getElementsByClassName('card-container')[0];
 
-cards.forEach(card => {
+function createCard(card, parent) {
     const cardElement = document.createElement('div');
     cardElement.className = "card";
-    const cardDetail = document.createElement('div');
-    cardDetail.className = "card-detail";
-    if (card.detail.p) {
+    if (card.detail) {
+        const cardDetail = document.createElement('div');
+        cardDetail.className = "card-detail";
+        if (card.detail.p) {
+            let cardP = document.createElement('p');
+            cardP.innerHTML = card.detail.p;
+            cardDetail.appendChild(cardP);
+        }
+        if (card.detail.img != null || card.detail.img != undefined) {
+            let cardImg = document.createElement('img');
+            cardImg.src = '../../assets/akar-icons_chevron-down.png';
+            cardDetail.appendChild(cardImg);
+        }
+        cardElement.appendChild(cardDetail);
+    }
+    if (card.description) {
+        const cardDesc = document.createElement('div');
+        cardDesc.className = "card-desc";
         let cardP = document.createElement('p');
         cardP.innerHTML = card.detail.p;
-        cardDetail.appendChild(cardP);
+        cardDesc.appendChild(cardP);
+        cardElement.appendChild(cardDesc);
     }
-    if (card.detail.img != null || card.detail.img != undefined) {
-        let cardImg = document.createElement('img');
-        cardImg.src = '../../assets/akar-icons_chevron-down.png';
-        cardDetail.appendChild(cardImg);
+    if (card.childrens) {
+        card.childrens.forEach(child => createCard(child, cardElement));
     }
-    cardElement.appendChild(cardDetail);
-    elements.push(cardElement);
-});
+    if (parent) {
+        cardElement.style.display = 'none';
+        parent.appendChild(cardElement);
+    }
+    else elements.push(cardElement);
+}
 
-let moreThan6 = false;
+cards.forEach(card => createCard(card));
 
 for (let index = 0; index < elements.length; index++) {
-    cardContainer.appendChild(elements[index]);
+    let card = elements[index];
     if (index > 5) {
-        elements[index].style.display = 'none';
-        moreThan6 = true;
+        card.style.display = 'none';
     }
+
+    card.addEventListener('click', e => {
+        let button = card.querySelector('* > img');
+        card.querySelectorAll('* > .card').forEach(child => {
+            child.style.display = (button.style.transform == '') ? 'block' : 'none';
+        });
+        button.style.transform = (button.style.transform == '') ? 'rotate(180deg)' : null;
+    });
+    cardContainer.appendChild(card);
 }
 
-if (moreThan6) {
-}
+document.getElementsByClassName('read-more-button')[0].addEventListener('click', e => {
+    for (let index = 6; index < elements.length; index++) {
+        elements[index].style.display = (isClicked) ? 'none' : 'block';
+    }
+    e.target.style.transform = (isClicked) ? 'rotate(0deg)' : 'rotate(180deg)';
+    isClicked = !isClicked;
+});
