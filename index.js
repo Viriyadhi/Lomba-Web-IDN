220a0488ab22ac7cb824721d151a5ddb3d4736d7
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2021-11-27 00:23:30 +0700

    fix(Navbar)[OF]: Fixing display not showing bug

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let links = document.querySelector('.navbar-links-container');
-links.style.display = 'none';
 
 document.querySelector('.navbar-title button').addEventListener('click', e => {
-    links.style.display = (links.style.display == 'none') ? 'flex' : 'none';
+    links.style.display = (links.style.display == 'none' || links.style.display == '') ? 'flex' : 'none';
 });
