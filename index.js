ed5a3e83a60f3ef86efcf2acaa6f730fdd4b575a
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2021-11-27 10:00:50 +0700

    feat(About Page)[OF]: Restyling About page

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const iframe = document.getElementsByTagName('iframe')[0];
 function setIframeSize() {
     const iframeContent = iframe.contentDocument; 
     const iframeBody = iframeContent.body;
-    const iframeDiv = iframeBody.querySelectorAll('body > div');
+    const iframeDiv = iframeBody.querySelectorAll('body > div, body > main');
     const iframeLastChildRect = iframeDiv[iframeDiv.length - 1].getBoundingClientRect();
 
     iframe.style.height = (iframeLastChildRect.y + iframeLastChildRect.height) + 'px';
@@ -18,3 +18,11 @@ function setIframeSize() {
 iframe.addEventListener('load' , setIframeSize);
 
 window.addEventListener('resize', setIframeSize);
+
+
+document.querySelectorAll('.navbar-links-container>a').forEach(link => {
+    link.addEventListener('click', e => {
+        document.querySelector('iframe').src = `pages/${link.getAttribute('href')}/${link.getAttribute('href')}.html`;
+        e.preventDefault();
+    });
+});
